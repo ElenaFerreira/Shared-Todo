3fe2e8dbc1d90f58abc5cf656d58a677b45141a5
DumbWays31 <elena.ferreira@free.fr>
2025-05-21 16:10:59 +0200

    fix bug

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -2,10 +2,10 @@ import { connectDB } from "@/lib/mongodb";
 import { Todo } from "@/models/Todo";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
   await connectDB();
 
-  const deleted = await Todo.findByIdAndDelete(context.params.id);
+  const deleted = await Todo.findByIdAndDelete(params.id);
   if (!deleted) {
     return NextResponse.json({ error: "Not found" }, { status: 404 });
   }
@@ -13,10 +13,10 @@ export async function DELETE(req: NextRequest, context: { params: { id: string }
   return NextResponse.json({ success: true });
 }
 
-export async function PATCH(req: NextRequest, context: { params: { id: string } }) {
+export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
   await connectDB();
 
-  const todo = await Todo.findById(context.params.id);
+  const todo = await Todo.findById(params.id);
   if (!todo) {
     return NextResponse.json({ error: "Not found" }, { status: 404 });
   }
