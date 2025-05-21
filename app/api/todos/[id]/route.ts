0f80137df9d8ced14937722a181cedfbc391994b
DumbWays31 <elena.ferreira@free.fr>
2025-05-21 16:34:57 +0200

    fix bug

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -2,29 +2,11 @@ import { connectDB } from "@/lib/mongodb";
 import { Todo } from "@/models/Todo";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+export async function PUT(req: NextRequest, context: any) {
   await connectDB();
 
   try {
-    const { id } = params;
-    const todo = await Todo.findById(id);
-    if (!todo) {
-      return NextResponse.json({ error: "Not found" }, { status: 404 });
-    }
-
-    await Todo.findByIdAndDelete(id);
-
-    return NextResponse.json({ success: true });
-  } catch (error) {
-    return NextResponse.json({ error: error }, { status: 500 });
-  }
-}
-
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
-  await connectDB();
-
-  try {
-    const { id } = params;
+    const id = context.params.id;
     const todo = await Todo.findById(id);
     if (!todo) {
       return NextResponse.json({ error: "Not found" }, { status: 404 });
@@ -34,6 +16,24 @@ export async function PUT(req: NextRequest, { params }: { params: { id: string }
     await todo.save();
 
     return NextResponse.json(todo);
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
+
+export async function DELETE(req: NextRequest, context: any) {
+  await connectDB();
+
+  try {
+    const id = context.params.id;
+    const todo = await Todo.findById(id);
+    if (!todo) {
+      return NextResponse.json({ error: "Not found" }, { status: 404 });
+    }
+
+    await Todo.findByIdAndDelete(id);
+
+    return NextResponse.json({ success: true });
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 500 });
   }
